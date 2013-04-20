a217eb7ca891db2c3b3f1bae492ee283ba8290e2
Bob Myers <bob@nextfort.com>
2013-04-20 09:55:39 -0700

    hw3-part2

--- features/sort_movie_list.feature
+++ features/sort_movie_list.feature
@@ -23,9 +23,15 @@ Background: movies have been added to database
 
 Scenario: sort movies alphabetically
   When I follow "Movie Title"
-  # your steps here
+  Then I should see "2001: A Space Odyssey" before "Aladdin"
+  And  I should see "Aladdin" before "Amelie"
+  And  I should see "Amelie" before "Chicken Run"
+
 
 Scenario: sort movies in increasing order of release date
   When I follow "Release Date"
-  # your steps here
+  Then I should see "2001: A Space Odyssey" before "Raiders of the Lost Ark"
+  Then I should see "Raiders of the Lost Ark" before "The Terminator"
+  Then I should see "The Terminator" before "When Harry Met Sally"
+
 
--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -2,16 +2,16 @@
 
 Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
-    # each returned element will be a hash whose key is the table header.
-    # you should arrange to add that movie to the database here.
+    Movie.create(movie)
   end
-  flunk "Unimplemented"
 end
 
 # Make sure that one string (regexp) occurs before or after another one
 #   on the same page
 
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
+  debugger
+  #page.body =~ /e1.*e2/
   #  ensure that that e1 occurs before e2.
   #  page.body is the entire content of the page as a string.
   flunk "Unimplemented"
@@ -22,7 +22,19 @@ end
 #  "When I check the following ratings: G"
 
 When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
-  # HINT: use String#split to split up the rating_list, then
-  #   iterate over the ratings and reuse the "When I check..." or
-  #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
+  rating_list.split(/, /).each do |rt|
+    step(%Q(I #{uncheck}check "ratings_#{rt}"))
+  end
+end
+
+Then /I should see all of the ([^ ]+)/ do |items|
+  items.classify.constantize.all.each do |m|
+    step(%Q(I should see "#{m[:title]}"))
+  end
+end
+
+Then /I should see none of the ([^ ]+)/ do |items|
+  items.classify.constantize.all.each do |m|
+    step(%Q(I should not see "#{m[:title]}"))
+  end
 end
